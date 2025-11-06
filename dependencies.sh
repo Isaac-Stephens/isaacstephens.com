d76c42bcade5a710487e45ebc5176bb290c18640
isaac <isaac@isaacDebHome>
2025-11-05 18:49:55 -0600

    updated dep

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt update
-sudo apt install python3-flask python3-flask-login python3-flask-sqlalchemy python3-mysql.connector python3-werkzeug python3-dotenv
+# sudo apt update
+# sudo apt install python3-flask python3-flask-login python3-flask-sqlalchemy python3-mysql.connector python3-werkzeug python3-dotenv
 
 # For a .venv environment uncomment bellow
-# python3 -m venv venv
-# source venv/bin/activate
-# pip install gunicorn flask mysql-connector-python python-dotenv
-# gunicorn --bind 127.0.0.1:5000 main:app
+python3 -m venv venv
+source venv/bin/activate
+pip install gunicorn flask mysql-connector-python python-dotenv
+gunicorn --bind 127.0.0.1:5000 main:app
