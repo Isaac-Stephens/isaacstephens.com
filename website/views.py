055afc514d27f2733a4afd1c0a7007de23826775
Isaac Stephens <198599277+Isaac-Stephens@users.noreply.github.com>
2026-08-26 15:59:13 -0500

    profile pic :)

--- website/views.py
+++ website/views.py
@@ -3,12 +3,16 @@
 # --------------------------------------------
 
 from flask import Blueprint, render_template
-
+from datetime import datetime
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template("home.html")
+
+# date for copyright
+def inject_now():
+    return {"now": datetime.now(datetime.timezone.utc)}
 
 # ++++++++++++++++++++++++
 # ======= Articles =======
